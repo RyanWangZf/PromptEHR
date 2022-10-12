545f1de54f58428473affc931c2505cee524826b
Zifeng Wang <zifengw2@illinois.edu>
2022-10-11 21:25:39 -0500

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(this_directory, 'requirements.txt'),
 
 setuptools.setup(
     name = 'PromptEHR',
-    version = '0.0.1a',
+    version = '0.0.1',
     author = 'Zifeng Wang',
     author_email = 'zifengw2@illinois.edu',
     description = 'EHR generation with prompt learning by language models.',
